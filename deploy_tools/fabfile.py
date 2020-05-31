6b74c6e63a6438f5956f8a0f9e3b5972edb6e763
cgoodfred <corey.goodfred@gmail.com>
2020-05-30 20:53:55 -0400

    fixed typo in function call in fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -9,7 +9,7 @@ def deploy():
 	run(f'mkdir -p {site_folder}')
 	with cd(site_folder):
 		_get_latest_source()
-		_update_virtlenv()
+		_update_virtualenv()
 		_create_or_update_dotenv()
 		_update_static_files()
 		_update_database()
